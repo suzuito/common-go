173f52d304234a4943bf7021258124b1c3e9434d
鈴木泰斗 <suzukitaito@suzukitaitonoMacBook-Pro.local>
2020-01-28 21:45:00 +0900

    Add readme and unit test

--- cgin/query_test.go
+++ cgin/query_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test(t *testing.T) {
+func TestInt(t *testing.T) {
 	testCases := []struct {
 		desc            string
 		inputQueryKey   string
@@ -18,21 +18,164 @@ func Test(t *testing.T) {
 		expected        int
 	}{
 		{
-			desc:            "",
+			desc:            "Convertable query",
 			inputQueryKey:   "q",
 			inputQueryValue: "1",
 			inputDefault:    100,
 			expected:        1,
 		},
+		{
+			desc:         "Empty query",
+			inputDefault: 100,
+			expected:     100,
+		},
+		{
+			desc:            "Unconvertable (str)",
+			inputQueryKey:   "q",
+			inputQueryValue: "hoge",
+			inputDefault:    100,
+			expected:        100,
+		},
 	}
 	for _, tC := range testCases {
 		t.Run(tC.desc, func(t *testing.T) {
 			req, _ := http.NewRequest("GET", "", nil)
-			req.URL.RawQuery = fmt.Sprintf("%s=%s", tC.inputQueryKey, tC.inputQueryValue)
+			if tC.inputQueryKey != "" {
+				req.URL.RawQuery = fmt.Sprintf("%s=%s", tC.inputQueryKey, tC.inputQueryValue)
+			}
 			ctx := gin.Context{
 				Request: req,
 			}
 			real := DefaultQueryAsInt(&ctx, tC.inputQueryKey, tC.inputDefault)
+			assert.Equal(t, tC.expected, real)
+		})
+	}
+}
+
+func TestInt64(t *testing.T) {
+	testCases := []struct {
+		desc            string
+		inputQueryKey   string
+		inputQueryValue string
+		inputDefault    int64
+		expected        int64
+	}{
+		{
+			desc:            "Convertable query",
+			inputQueryKey:   "q",
+			inputQueryValue: "1",
+			inputDefault:    100,
+			expected:        1,
+		},
+		{
+			desc:         "Empty query",
+			inputDefault: 100,
+			expected:     100,
+		},
+		{
+			desc:            "Unconvertable (str)",
+			inputQueryKey:   "q",
+			inputQueryValue: "hoge",
+			inputDefault:    100,
+			expected:        100,
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			req, _ := http.NewRequest("GET", "", nil)
+			if tC.inputQueryKey != "" {
+				req.URL.RawQuery = fmt.Sprintf("%s=%s", tC.inputQueryKey, tC.inputQueryValue)
+			}
+			ctx := gin.Context{
+				Request: req,
+			}
+			real := DefaultQueryAsInt64(&ctx, tC.inputQueryKey, tC.inputDefault)
+			assert.Equal(t, tC.expected, real)
+		})
+	}
+}
+
+func TestFloat32(t *testing.T) {
+	testCases := []struct {
+		desc            string
+		inputQueryKey   string
+		inputQueryValue string
+		inputDefault    float32
+		expected        float32
+	}{
+		{
+			desc:            "Convertable query",
+			inputQueryKey:   "q",
+			inputQueryValue: "1",
+			inputDefault:    100,
+			expected:        1,
+		},
+		{
+			desc:         "Empty query",
+			inputDefault: 100,
+			expected:     100,
+		},
+		{
+			desc:            "Unconvertable (str)",
+			inputQueryKey:   "q",
+			inputQueryValue: "hoge",
+			inputDefault:    100,
+			expected:        100,
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			req, _ := http.NewRequest("GET", "", nil)
+			if tC.inputQueryKey != "" {
+				req.URL.RawQuery = fmt.Sprintf("%s=%s", tC.inputQueryKey, tC.inputQueryValue)
+			}
+			ctx := gin.Context{
+				Request: req,
+			}
+			real := DefaultQueryAsFloat32(&ctx, tC.inputQueryKey, tC.inputDefault)
+			assert.Equal(t, tC.expected, real)
+		})
+	}
+}
+
+func TestFloat64(t *testing.T) {
+	testCases := []struct {
+		desc            string
+		inputQueryKey   string
+		inputQueryValue string
+		inputDefault    float64
+		expected        float64
+	}{
+		{
+			desc:            "Convertable query",
+			inputQueryKey:   "q",
+			inputQueryValue: "1",
+			inputDefault:    100,
+			expected:        1,
+		},
+		{
+			desc:         "Empty query",
+			inputDefault: 100,
+			expected:     100,
+		},
+		{
+			desc:            "Unconvertable (str)",
+			inputQueryKey:   "q",
+			inputQueryValue: "hoge",
+			inputDefault:    100,
+			expected:        100,
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			req, _ := http.NewRequest("GET", "", nil)
+			if tC.inputQueryKey != "" {
+				req.URL.RawQuery = fmt.Sprintf("%s=%s", tC.inputQueryKey, tC.inputQueryValue)
+			}
+			ctx := gin.Context{
+				Request: req,
+			}
+			real := DefaultQueryAsFloat64(&ctx, tC.inputQueryKey, tC.inputDefault)
 			assert.Equal(t, tC.expected, real)
 		})
 	}
