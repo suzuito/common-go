e876ee529b89560f522e3465e3c97dc6d3e5395c
鈴木泰斗 <suzukitaito@suzukitaitos-MacBook-Pro.local>
2020-02-13 23:35:27 +0900

    Save

--- cgin/application.go
+++ cgin/application.go
@@ -2,6 +2,7 @@ package cgin
 
 import (
 	"context"
+	env "github.com/suzuito/common-env"
 	"os"
 	"strings"
 )
@@ -9,16 +10,25 @@ import (
 // ApplicationGin ...
 type ApplicationGin interface {
 	AllowedOrigins() []string
+	AllowedMethods() []string
+	AllowedHeaders() []string
+	AllowedCredential() bool
 }
 
 // ApplicationGinImpl ...
 type ApplicationGinImpl struct {
-	allowedOrigins []string
+	allowedOrigins    []string
+	allowedMethods    []string
+	allowedHeaders    []string
+	allowedCredential bool
 }
 
 // NewApplicationGinImpl ...
 func NewApplicationGinImpl(ctx context.Context) (*ApplicationGinImpl, error) {
 	return &ApplicationGinImpl{
-		allowedOrigins: strings.Split(os.Getenv("ALLOWED_ORIGINS"), ","),
+		allowedOrigins:    strings.Split(os.Getenv("ALLOWED_ORIGINS"), ","),
+		allowedMethods:    strings.Split(os.Getenv("ALLOWED_METHODS"), ","),
+		allowedHeaders:    strings.Split(os.Getenv("ALLOWED_HEADERS"), ","),
+		allowedCredential: env.GetenvAsBool("ALLOWED_CREDENTIAL", false),
 	}, nil
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.13
 require (
 	cloud.google.com/go/firestore v1.1.1
 	firebase.google.com/go v3.12.0+incompatible
+	github.com/gin-contrib/cors v1.3.0
 	github.com/gin-gonic/gin v1.5.0
 	github.com/stretchr/testify v1.4.0
+	github.com/suzuito/common-env v1.0.1
 )
