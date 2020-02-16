b0fbe526f1abcd4f686bc376a3775c4b03a32341
鈴木泰斗 <suzukitaito@suzukitaitos-MacBook-Pro.local>
2020-02-16 13:08:32 +0900

    Add new mocks and fix helper method bugs

--- cgcp/cweb/helper.go
+++ cgcp/cweb/helper.go
@@ -1,22 +1,14 @@
 package cweb
 
 import (
-	"context"
 	"net/http"
 
-	"cloud.google.com/go/firestore"
-	"firebase.google.com/go/auth"
 	"github.com/gin-gonic/gin"
 	"github.com/suzuito/common-go/application"
+	"github.com/suzuito/common-go/cgcp"
 	"github.com/suzuito/common-go/cgin"
 	"github.com/suzuito/common-go/clogger"
 )
-
-// FirebaseApp ...
-type FirebaseApp interface {
-	Firestore(ctx context.Context) (*firestore.Client, error)
-	Auth(ctx context.Context) (*auth.Client, error)
-}
 
 // HO ...
 type HO struct {
@@ -29,32 +21,29 @@ type HO struct {
 func H(
 	ctx *gin.Context,
 	app application.ApplicationLogger,
-	appFirebase FirebaseApp,
+	appFirebase cgcp.FirebaseApp,
 	proc func(
 		logger clogger.Logger,
-		fcli *firestore.Client,
-		fauth *auth.Client,
+		fcli cgcp.FirebaseFirestoreClient,
+		fauth cgcp.FirebaseAuthClient,
 	) error,
 	opt *HO,
 ) {
 	var logger clogger.Logger
-	var fcli *firestore.Client
-	var fauth *auth.Client
+	var fcli cgcp.FirebaseFirestoreClient
+	var fauth cgcp.FirebaseAuthClient
 	var err error
 	if opt == nil || opt.LoggerNotUse == false {
 		logger = app.Logger(ctx)
 		defer logger.Close()
 	}
 	if opt == nil || opt.FirestoreClientNotUse == false || opt.AuthClientNotUse == false {
-		if err != nil {
-			logger.Errorf("%+v", err)
-			cgin.Abort(ctx, cgin.NewHTTPError(http.StatusInternalServerError, "InternalServerError", err))
-			return
-		}
 		if opt == nil || opt.FirestoreClientNotUse == false {
 			fcli, err = appFirebase.Firestore(ctx)
 			if err != nil {
-				logger.Errorf("%+v", err)
+				if logger != nil {
+					logger.Errorf("%+v", err)
+				}
 				cgin.Abort(ctx, cgin.NewHTTPError(http.StatusInternalServerError, "InternalServerError", err))
 				return
 			}
@@ -63,12 +52,16 @@ func H(
 		if opt == nil || opt.AuthClientNotUse == false {
 			fauth, err = appFirebase.Auth(ctx)
 			if err != nil {
-				logger.Errorf("%+v", err)
+				if logger != nil {
+					logger.Errorf("%+v", err)
+				}
 				cgin.Abort(ctx, cgin.NewHTTPError(http.StatusInternalServerError, "InternalServerError", err))
 				return
 			}
 		}
-		if err := proc(logger, fcli, fauth); err != nil {
+	}
+	if err := proc(logger, fcli, fauth); err != nil {
+		if logger != nil {
 			logger.Errorf("%+v", err)
 		}
 	}
--- cgcp/cweb/helper_test.go
+++ cgcp/cweb/helper_test.go
@@ -1,70 +1,300 @@
 package cweb
 
 import (
+	"fmt"
+	"net/http"
 	"net/http/httptest"
 	"testing"
 
-	"cloud.google.com/go/firestore"
-	"firebase.google.com/go/auth"
 	"github.com/gin-gonic/gin"
 	"github.com/golang/mock/gomock"
-	"github.com/suzuito/common-go/application_mock"
+	"github.com/suzuito/common-go/application"
+	"github.com/suzuito/common-go/cgcp"
 	"github.com/suzuito/common-go/clogger"
-	"github.com/suzuito/common-go/clogger_mock"
+	"gopkg.in/go-playground/assert.v1"
 )
 
 func Test(t *testing.T) {
 	testCases := []struct {
 		desc      string
 		setUpMock func(
-			app *application_mock.MockApplicationLogger,
-			logger *clogger_mock.MockLogger,
-			appFirebase *cweb_mock.MockAppFirebaseWrapper,
+			app *application.MockApplicationLogger,
+			logger *clogger.MockLogger,
+			appFirebase *cgcp.MockFirebaseApp,
+			dcliFirebase *cgcp.MockFirebaseFirestoreClient,
+			acliFirebase *cgcp.MockFirebaseAuthClient,
 		)
 		inputProc func(
 			logger clogger.Logger,
-			fcli *firestore.Client,
-			fauth *auth.Client,
+			fcli cgcp.FirebaseFirestoreClient,
+			fauth cgcp.FirebaseAuthClient,
 		) error
-		inputOpt *HO
+		inputOpt         *HO
+		expectedResponse *http.Response
 	}{
 		{
-			desc: "",
+			desc: "If opt is nil",
 			setUpMock: func(
-				app *application_mock.MockApplicationLogger,
-				logger *clogger_mock.MockLogger,
-				appFirebase *cweb_mock.MockAppFirebaseWrapper,
+				app *application.MockApplicationLogger,
+				logger *clogger.MockLogger,
+				appFirebase *cgcp.MockFirebaseApp,
+				dcliFirebase *cgcp.MockFirebaseFirestoreClient,
+				acliFirebase *cgcp.MockFirebaseAuthClient,
 			) {
 				app.
-					EXPECT().
-					Logger(gomock.Any()).
-					Return(logger)
+					EXPECT().Logger(gomock.Any()).Return(logger)
 				logger.
-					EXPECT().
-					Close()
+					EXPECT().Close()
+				appFirebase.
+					EXPECT().Firestore(gomock.Any()).Return(dcliFirebase, nil)
+				dcliFirebase.
+					EXPECT().Close()
+				appFirebase.
+					EXPECT().Auth(gomock.Any()).Return(acliFirebase, nil)
 			},
 			inputProc: func(
 				logger clogger.Logger,
-				fcli *firestore.Client,
-				fauth *auth.Client,
+				fcli cgcp.FirebaseFirestoreClient,
+				fauth cgcp.FirebaseAuthClient,
 			) error {
 				return nil
 			},
 			inputOpt: nil,
+		},
+		{
+			desc: "Use all",
+			setUpMock: func(
+				app *application.MockApplicationLogger,
+				logger *clogger.MockLogger,
+				appFirebase *cgcp.MockFirebaseApp,
+				dcliFirebase *cgcp.MockFirebaseFirestoreClient,
+				acliFirebase *cgcp.MockFirebaseAuthClient,
+			) {
+				app.
+					EXPECT().Logger(gomock.Any()).Return(logger)
+				logger.
+					EXPECT().Close()
+				appFirebase.
+					EXPECT().Firestore(gomock.Any()).Return(dcliFirebase, nil)
+				dcliFirebase.
+					EXPECT().Close()
+				appFirebase.
+					EXPECT().Auth(gomock.Any()).Return(acliFirebase, nil)
+			},
+			inputProc: func(
+				logger clogger.Logger,
+				fcli cgcp.FirebaseFirestoreClient,
+				fauth cgcp.FirebaseAuthClient,
+			) error {
+				return nil
+			},
+			inputOpt: &HO{
+				FirestoreClientNotUse: false,
+				AuthClientNotUse:      false,
+				LoggerNotUse:          false,
+			},
+		},
+		{
+			desc: "Use firestore client only",
+			setUpMock: func(
+				app *application.MockApplicationLogger,
+				logger *clogger.MockLogger,
+				appFirebase *cgcp.MockFirebaseApp,
+				dcliFirebase *cgcp.MockFirebaseFirestoreClient,
+				acliFirebase *cgcp.MockFirebaseAuthClient,
+			) {
+				appFirebase.
+					EXPECT().Firestore(gomock.Any()).Return(dcliFirebase, nil)
+				dcliFirebase.
+					EXPECT().Close()
+			},
+			inputProc: func(
+				logger clogger.Logger,
+				fcli cgcp.FirebaseFirestoreClient,
+				fauth cgcp.FirebaseAuthClient,
+			) error {
+				return nil
+			},
+			inputOpt: &HO{
+				FirestoreClientNotUse: false,
+				AuthClientNotUse:      true,
+				LoggerNotUse:          true,
+			},
+		},
+		{
+			desc: "Use firestore auth only",
+			setUpMock: func(
+				app *application.MockApplicationLogger,
+				logger *clogger.MockLogger,
+				appFirebase *cgcp.MockFirebaseApp,
+				dcliFirebase *cgcp.MockFirebaseFirestoreClient,
+				acliFirebase *cgcp.MockFirebaseAuthClient,
+			) {
+				appFirebase.
+					EXPECT().Auth(gomock.Any()).Return(acliFirebase, nil)
+			},
+			inputProc: func(
+				logger clogger.Logger,
+				fcli cgcp.FirebaseFirestoreClient,
+				fauth cgcp.FirebaseAuthClient,
+			) error {
+				return nil
+			},
+			inputOpt: &HO{
+				FirestoreClientNotUse: true,
+				AuthClientNotUse:      false,
+				LoggerNotUse:          true,
+			},
+		},
+		{
+			desc: "Use firestore logger only",
+			setUpMock: func(
+				app *application.MockApplicationLogger,
+				logger *clogger.MockLogger,
+				appFirebase *cgcp.MockFirebaseApp,
+				dcliFirebase *cgcp.MockFirebaseFirestoreClient,
+				acliFirebase *cgcp.MockFirebaseAuthClient,
+			) {
+				app.
+					EXPECT().Logger(gomock.Any()).Return(logger)
+				logger.
+					EXPECT().Close()
+			},
+			inputProc: func(
+				logger clogger.Logger,
+				fcli cgcp.FirebaseFirestoreClient,
+				fauth cgcp.FirebaseAuthClient,
+			) error {
+				return nil
+			},
+			inputOpt: &HO{
+				FirestoreClientNotUse: true,
+				AuthClientNotUse:      true,
+				LoggerNotUse:          false,
+			},
+		},
+		{
+			desc: "Error Firestore",
+			setUpMock: func(
+				app *application.MockApplicationLogger,
+				logger *clogger.MockLogger,
+				appFirebase *cgcp.MockFirebaseApp,
+				dcliFirebase *cgcp.MockFirebaseFirestoreClient,
+				acliFirebase *cgcp.MockFirebaseAuthClient,
+			) {
+				app.
+					EXPECT().Logger(gomock.Any()).Return(logger)
+				logger.
+					EXPECT().Close()
+				appFirebase.
+					EXPECT().Firestore(gomock.Any()).Return(dcliFirebase, fmt.Errorf("Dummy error"))
+				logger.
+					EXPECT().Errorf(gomock.Any(), gomock.Any())
+			},
+			inputProc: func(
+				logger clogger.Logger,
+				fcli cgcp.FirebaseFirestoreClient,
+				fauth cgcp.FirebaseAuthClient,
+			) error {
+				return nil
+			},
+			inputOpt: &HO{
+				FirestoreClientNotUse: false,
+				AuthClientNotUse:      true,
+				LoggerNotUse:          false,
+			},
+			expectedResponse: &http.Response{
+				StatusCode: http.StatusInternalServerError,
+			},
+		},
+		{
+			desc: "Error Auth",
+			setUpMock: func(
+				app *application.MockApplicationLogger,
+				logger *clogger.MockLogger,
+				appFirebase *cgcp.MockFirebaseApp,
+				dcliFirebase *cgcp.MockFirebaseFirestoreClient,
+				acliFirebase *cgcp.MockFirebaseAuthClient,
+			) {
+				app.
+					EXPECT().Logger(gomock.Any()).Return(logger)
+				logger.
+					EXPECT().Close()
+				appFirebase.
+					EXPECT().Auth(gomock.Any()).Return(acliFirebase, fmt.Errorf("Dummy error"))
+				logger.
+					EXPECT().Errorf(gomock.Any(), gomock.Any())
+			},
+			inputProc: func(
+				logger clogger.Logger,
+				fcli cgcp.FirebaseFirestoreClient,
+				fauth cgcp.FirebaseAuthClient,
+			) error {
+				return nil
+			},
+			inputOpt: &HO{
+				FirestoreClientNotUse: true,
+				AuthClientNotUse:      false,
+				LoggerNotUse:          false,
+			},
+			expectedResponse: &http.Response{
+				StatusCode: http.StatusInternalServerError,
+			},
+		},
+		{
+			desc: "Error proc",
+			setUpMock: func(
+				app *application.MockApplicationLogger,
+				logger *clogger.MockLogger,
+				appFirebase *cgcp.MockFirebaseApp,
+				dcliFirebase *cgcp.MockFirebaseFirestoreClient,
+				acliFirebase *cgcp.MockFirebaseAuthClient,
+			) {
+				app.
+					EXPECT().Logger(gomock.Any()).Return(logger)
+				logger.
+					EXPECT().Close()
+				logger.
+					EXPECT().Errorf(gomock.Any(), gomock.Any())
+			},
+			inputProc: func(
+				logger clogger.Logger,
+				fcli cgcp.FirebaseFirestoreClient,
+				fauth cgcp.FirebaseAuthClient,
+			) error {
+				return fmt.Errorf("Dummy error")
+			},
+			inputOpt: &HO{
+				FirestoreClientNotUse: true,
+				AuthClientNotUse:      true,
+				LoggerNotUse:          false,
+			},
 		},
 	}
 	for _, tC := range testCases {
 		t.Run(tC.desc, func(t *testing.T) {
 			ctrlApp := gomock.NewController(t)
 			defer ctrlApp.Finish()
-			app := application_mock.NewMockApplicationLogger(ctrlApp)
+			app := application.NewMockApplicationLogger(ctrlApp)
 			ctrlLogger := gomock.NewController(t)
 			defer ctrlLogger.Finish()
-			logger := clogger_mock.NewMockLogger(ctrlLogger)
+			logger := clogger.NewMockLogger(ctrlLogger)
 			ctrlAppFirebase := gomock.NewController(t)
 			defer ctrlAppFirebase.Finish()
-			appFirebase := cweb_mock.NewMockAppFirebaseWrapper(ctrlAppFirebase)
-			tC.setUpMock(app, logger, appFirebase)
+			appFirebase := cgcp.NewMockFirebaseApp(ctrlAppFirebase)
+			ctrlFirestoreFirebase := gomock.NewController(t)
+			defer ctrlFirestoreFirebase.Finish()
+			cliFirestoreFirebase := cgcp.NewMockFirebaseFirestoreClient(ctrlFirestoreFirebase)
+			ctrlAuthFirebase := gomock.NewController(t)
+			defer ctrlAuthFirebase.Finish()
+			cliAuthFirebase := cgcp.NewMockFirebaseAuthClient(ctrlAuthFirebase)
+			tC.setUpMock(
+				app,
+				logger,
+				appFirebase,
+				cliFirestoreFirebase,
+				cliAuthFirebase,
+			)
 			// _, _ := http.NewRequest("GET", "/dummy", nil)
 			rec := httptest.NewRecorder()
 			ctx, _ := gin.CreateTestContext(rec)
@@ -75,6 +305,19 @@ func Test(t *testing.T) {
 				tC.inputProc,
 				tC.inputOpt,
 			)
+			result := rec.Result()
+			if result.StatusCode != http.StatusOK {
+				assert.NotEqual(
+					t,
+					nil,
+					tC.expectedResponse,
+				)
+				assert.Equal(
+					t,
+					tC.expectedResponse.StatusCode,
+					result.StatusCode,
+				)
+			}
 		})
 	}
 }
