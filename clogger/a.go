5d6ccc09af569e6c216879cdaa4ccf7a4f843a9c
鈴木泰斗 <suzukitaito@suzukitaitos-MacBook-Pro.local>
2020-03-14 12:58:47 +0900

    Add logger 4

--- clogger/a.go
+++ clogger/a.go
@@ -30,10 +30,3 @@ func (l *LoggerPrint) Errorf(format string, a ...interface{}) {
 	_, fname, line, _ := runtime.Caller(1)
 	fmt.Fprintf(os.Stderr, format+fmt.Sprintf(" (%s:%d)\n", fname, line), a...)
 }
-
-// Request ...
-func (l *LoggerPrint) Request(req *http.Request) {}
-
-// Close ...
-func (l *LoggerPrint) Close() {
-}
