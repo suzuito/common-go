a66da525d63be582de0bc891bfaab53107cd6185
鈴木泰斗 <suzukitaito@ip-192-168-11-3.ap-northeast-1.compute.internal>
2021-01-19 21:08:51 +0900

    Add sql url generator

---
+++ cdb/a.go
@@ -0,0 +1,27 @@
+package cdb
+
+import (
+	"fmt"
+	"net/url"
+	"path"
+)
+
+// NewTCPMySQLURLString generate a mysql url from src url
+// src url
+//   mysql://<user>:<passwd>@<host>/<db>?<query>
+// mysql url
+//   <user>:<passwd>@tcp(<host>)/<db>?<query>
+func NewTCPMySQLURLString(u *url.URL) string {
+	password, exists := u.User.Password()
+	if !exists {
+		password = ""
+	}
+	return fmt.Sprintf(
+		"%s:%s@tcp(%s)/%s?%s",
+		u.User.Username(),
+		password,
+		u.Host,
+		path.Base((u.Path)),
+		u.RawQuery,
+	)
+}
