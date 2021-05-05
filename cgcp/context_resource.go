425fe672ae458bb1bc87c59dacc7388a807ef7d1
鈴木泰斗 <suzukitaito@suzukitaitonoMacBook-Pro.local>
2021-05-06 00:50:21 +0900

    Add firestore

--- cgcp/context_resource.go
+++ cgcp/context_resource.go
@@ -3,13 +3,16 @@ package cgcp
 import (
 	"context"
 
+	"cloud.google.com/go/firestore"
 	"cloud.google.com/go/storage"
+	firebase "firebase.google.com/go"
 	"github.com/go-redis/redis/v7"
 	"golang.org/x/xerrors"
 )
 
 type GCPContextResource struct {
 	GCS *storage.Client
+	GCF *firestore.Client
 	GMS MemoryStoreClient
 }
 
@@ -25,11 +28,17 @@ func (r *GCPContextResource) Close() []error {
 			errs = append(errs, err)
 		}
 	}
+	if r.GCF != nil {
+		if err := r.GCF.Close(); err != nil {
+			errs = append(errs, err)
+		}
+	}
 	return errs
 }
 
 type GCPContextResourceGenerator struct {
 	newGCS      bool
+	newGCF      bool
 	newGMS      bool
 	redisClient *redis.Client
 	redisTTL    int
@@ -39,6 +48,7 @@ func NewGCPContextResourceGenerator() *GCPContextResourceGenerator {
 	return &GCPContextResourceGenerator{
 		newGCS:      false,
 		newGMS:      false,
+		newGCF:      false,
 		redisClient: nil,
 	}
 }
@@ -53,6 +63,10 @@ func (r *GCPContextResourceGenerator) GMS(cli *redis.Client, ttl int) {
 	r.newGMS = true
 }
 
+func (r *GCPContextResourceGenerator) GCF() {
+	r.newGCF = true
+}
+
 func (r *GCPContextResourceGenerator) Gen(ctx context.Context) (*GCPContextResource, error) {
 	var err error
 	ret := GCPContextResource{}
@@ -65,6 +79,19 @@ func (r *GCPContextResourceGenerator) Gen(ctx context.Context) (*GCPContextResou
 		ret.GCS, err = storage.NewClient(ctx)
 		if err != nil {
 			return nil, xerrors.Errorf("Cannot storage.NewClient : %w", err)
+		}
+	}
+	if r.newGCF {
+		var app *firebase.App
+		app, err = firebase.NewApp(ctx, nil)
+		if err != nil {
+			return nil, xerrors.Errorf("Cannot firebase.NewApp : %w", err)
+		}
+		if r.newGCF {
+			ret.GCF, err = app.Firestore(ctx)
+			if err != nil {
+				return nil, xerrors.Errorf("Cannot app.Firestore : %w", err)
+			}
 		}
 	}
 	if r.newGMS {
