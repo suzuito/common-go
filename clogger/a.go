a43323884e7368e63c4f762df78ffa48d803914b
鈴木泰斗 <suzukitaito@suzukitaitonoMacBook-Pro.local>
2021-05-08 02:01:38 +0900

    Add logger id

--- clogger/a.go
+++ clogger/a.go
@@ -17,22 +17,22 @@ type LoggerPrint struct {
 	ID string
 }
 
-// Infof ...
-func (l *LoggerPrint) Infof(format string, a ...interface{}) {
-	_, fname, line, _ := runtime.Caller(1)
+func (l *LoggerPrint) msg(level string, format string, a ...interface{}) string {
+	_, fname, line, _ := runtime.Caller(2)
 	s := fmt.Sprintf(format+fmt.Sprintf(" (%s:%d)\n", fname, line), a...)
+	s = level + " " + s
 	if l.ID != "" {
 		s = l.ID + " " + s
 	}
-	fmt.Fprintf(os.Stdout, s)
+	return s
+}
+
+// Infof ...
+func (l *LoggerPrint) Infof(format string, a ...interface{}) {
+	fmt.Fprintf(os.Stdout, l.msg("info", format, a...))
 }
 
 // Errorf ...
 func (l *LoggerPrint) Errorf(format string, a ...interface{}) {
-	_, fname, line, _ := runtime.Caller(1)
-	s := fmt.Sprintf(format+fmt.Sprintf(" (%s:%d)\n", fname, line), a...)
-	if l.ID != "" {
-		s = l.ID + " " + s
-	}
-	fmt.Fprintf(os.Stderr, s)
+	fmt.Fprintf(os.Stderr, l.msg("info", format, a...))
 }
